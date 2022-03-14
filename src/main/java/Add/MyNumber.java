1c2d8a519847beca4eba19f854a8be2127d16c72
pshivpuje <87057583+pshivpuje@users.noreply.github.com>
2022-03-14 16:28:22 +0530

    Update MyNumber.java

--- src/main/java/Add/MyNumber.java
+++ src/main/java/Add/MyNumber.java
@@ -32,4 +32,45 @@ public class MyNumber {
 	      this.number /= rhs.number;
 	      return this;
 	   }
+	
+	// Bugs 
+	
+	public static void main(String[] args) {
+			int caseIndex = 0;
+	    	switch (caseIndex) {
+	        	case 0:
+	            	System.out.println("Zero");
+	        	case 1:
+	            	System.out.println("One");
+	            	break;
+	        	case 2:
+	            	System.out.println("Two");
+	            	break;
+	        	default:
+	            	System.out.println("Default");
+	    	}
+		}
+	   
+		 public void dosomething() {
+				Integer number = null;
+				
+				if(number > 0)
+				{
+				   System.out.println("positive number");
+				}
+				
+				}
+		 public int divide (int numerator) {
+				return numerator/0;
+			}
+				public int addone (int number) {
+					return dosomethingprivate(number);
+				}
+				private int dosomethingprivate(int number) {
+				
+					return number++;
+				}
+	
+	
+	}
 	}
