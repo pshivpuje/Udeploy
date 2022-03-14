cc744f23cbb3c4554b863c3f6c694e61a73e7126
pshivpuje <87057583+pshivpuje@users.noreply.github.com>
2022-03-14 16:37:43 +0530

    Update MyNumber.java

--- src/main/java/Add/MyNumber.java
+++ src/main/java/Add/MyNumber.java
@@ -40,6 +40,42 @@ public class MyNumber {
 			      System.out.println("Access granted - You are old enough!"); 
 			    }
 			 } 
+	 
+	   public static void main(String[] args) {
+			int caseIndex = 0;
+	    	switch (caseIndex) {
+	        	case 0:
+	            	System.out.println("Zero");
+	        	case 1:
+	            	System.out.println("One");
+	            	break;
+	        	case 2:
+	            	System.out.println("Two");
+	            	break;
+	        	default:
+	            	System.out.println("Default");
+	    	}
+		}
+	   
+		 public void dosomething() {
+				Integer number = null;
+				
+				if(number > 0)
+				{
+				   System.out.println("positive number");
+				}
+				
+				}
+		 public int divide (int numerator) {
+				return numerator/0;
+			}
+				public int addone (int number) {
+					return dosomethingprivate(number);
+				}
+				private int dosomethingprivate(int number) {
+				
+					return number++;
+				}
 	
 	}
 	
