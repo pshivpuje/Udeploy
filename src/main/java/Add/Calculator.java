b4fcdc793ca3869b52f0ff7a78703d7e9f58dc2c
pshivpuje <87057583+pshivpuje@users.noreply.github.com>
2022-03-14 15:23:35 +0530

    Update Calculator.java

--- src/main/java/Add/Calculator.java
+++ src/main/java/Add/Calculator.java
@@ -28,4 +28,54 @@ public class Calculator {
 	      }
 	      return (double) number1 / number2;
 	   }
+	
+	// Adding Bugs 
+	
+	private static void buggy() throws IOException {
+		    try(FileInputStream input = new FileInputStream("file.txt")) {
+		        int data = input.read();
+		        while(data != -1){
+		            System.out.print((char) data);
+		            data = input.read();
+		        }
+		    }
+		}
+	   
+		public static void main(String[] args) {
+			int caseIndex = 0;
+	    	switch (caseIndex) {
+	        	case 0:
+	            	System.out.println("Zero");
+	        	case 1:
+	            	System.out.println("One");
+	            	break;
+	        	case 2:
+	            	System.out.println("Two");
+	            	break;
+	        	default:
+	            	System.out.println("Default");
+	    	}
+		}
+	   
+		 public void dosomething() {
+				Integer number = null;
+				
+				if(number > 0)
+				{
+				   System.out.println("positive number");
+				}
+				
+				}
+		 public int divide (int numerator) {
+				return numerator/0;
+			}
+				public int addone (int number) {
+					return dosomethingprivate(number);
+				}
+				private int dosomethingprivate(int number) {
+				
+					return number++;
+				}
+	
+	
 	}
