4e4431a3b0d37df9c4fa737789de5a6747f2af8a
pshivpuje <87057583+pshivpuje@users.noreply.github.com>
2022-03-14 16:35:43 +0530

    Update MyNumber.java

--- src/main/java/Add/MyNumber.java
+++ src/main/java/Add/MyNumber.java
@@ -32,45 +32,14 @@ public class MyNumber {
 	      this.number /= rhs.number;
 	      return this;
 	   }
-	
-	// Bugs 
-	
-	public static void main(String[] args) {
-			int caseIndex = 0;
-	    	switch (caseIndex) {
-	        	case 0:
-	            	System.out.println("Zero");
-	        	case 1:
-	            	System.out.println("One");
-	            	break;
-	        	case 2:
-	            	System.out.println("Two");
-	            	break;
-	        	default:
-	            	System.out.println("Default");
-	    	}
-		}
-	   
-		 public void dosomething() {
-				Integer number = null;
-				
-				if(number > 0)
-				{
-				   System.out.println("positive number");
-				}
-				
-				}
-		 public int divide (int numerator) {
-				return numerator/0;
-			}
-				public int addone (int number) {
-					return dosomethingprivate(number);
-				}
-				private int dosomethingprivate(int number) {
-				
-					return number++;
-				}
-	
+	// Denie access when age in less than 18
+	 static void checkAge(int age) { 
+			    if (age < 18) {
+			      throw new ArithmeticException("Access denied - You must be at least 18 years old."); 
+			    } else {
+			      System.out.println("Access granted - You are old enough!"); 
+			    }
+			 } 
 	
 	}
 	
